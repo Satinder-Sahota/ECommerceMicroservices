c32b14e2af515e927bba55706d15626190d66b26
DESKTOP-NVA7269\Satinder Sahota <akamal@finelinesettings.com>
2025-01-08 19:03:14 -0800

    Changes in Product Service and Cart Service added newly

---
+++ ProductService/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductService.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } // Primary Key
+        public string Name { get; set; } = string.Empty; // Product name
+        public string Description { get; set; } = string.Empty; // Product description
+        public decimal Price { get; set; } // Product price
+        public int Stock { get; set; } // Available stock
+    }
+}
--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ProductService.Data;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ProductContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -12,6 +18,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(opts=>
+    {
+        opts.WithTitle("Microservice Demo").
+        WithTheme(ScalarTheme.Mars).
+        WithDefaultHttpClient(ScalarTarget.CSharp,ScalarClient.HttpClient)
+        ;
+    });
+    //app.UseSwaggerUI(options =>
+    //{
+    //    options.SwaggerEndpoint("/openapi/v1.json","Microservices Demo");
+    //});
 }
 
 app.UseHttpsRedirection();
